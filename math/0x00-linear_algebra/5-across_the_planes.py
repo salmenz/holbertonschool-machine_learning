60738ed130c5cfa544e66784eb0f184741fca42e
salmenz <salmen.zooro@gmail.com>
2020-07-13 23:11:30 +0100

    5-6-7

---
+++ math/0x00-linear_algebra/5-across_the_planes.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+def add_matrices2D(mat1, mat2):
+    if len(mat2) != len(mat1):
+        return None
+    m = []
+    for i in range(len(mat1)):
+        if len(mat1[i]) != len(mat2[i]):
+            return None
+        l = []
+        for j in range(len(mat1[i])):
+            l.append(mat2[i][j]+mat1[i][j])
+        m.append(l)
+    return(m)
+    
+    
---
+++ math/0x00-linear_algebra/7-gettin_cozy.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python3
+def cat_matrices2D(mat1, mat2, axis=0):
+    
