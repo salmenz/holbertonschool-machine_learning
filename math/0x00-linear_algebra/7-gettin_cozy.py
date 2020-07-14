5e5923efdc7544cf51ac0f69f23deb3d4eb43946
salmenz <salmen.zooro@gmail.com>
2020-07-14 20:22:38 +0100

    com

--- math/0x00-linear_algebra/7-gettin_cozy.py
+++ math/0x00-linear_algebra/7-gettin_cozy.py
@@ -1,3 +1,14 @@
 #!/usr/bin/env python3
+from copy import deepcopy
 def cat_matrices2D(mat1, mat2, axis=0):
-    
+    m1 = []
+    m2 = []
+    m1 = deepcopy(mat1)
+    m2 = deepcopy(mat2)
+    if axis == 0:
+        return m1+m2
+    else:
+        for i in range(len(m1)):
+            for j in range(axis):
+                m1[i].append(m2[i][j])
+        return m1
---
+++ math/0x00-linear_algebra/8-ridin_bareback.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+import numpy as np
+def mat_mul(mat1, mat2):
+    for l in mat1:
+        if len(l)!=len(mat1[0]):
+            return None
+    if len(mat1[0]) != len (mat2):
+        return None
+    a = np.array(mat1)
+    b = np.array(mat2)
+    return np.matmul(a, b)
