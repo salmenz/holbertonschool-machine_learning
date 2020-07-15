1a981a5f99e806da7e7c12d09a48dbdcbfa6138b
salmenz <salmen.zooro@gmail.com>
2020-07-15 14:28:04 +0100

    fix

--- math/0x00-linear_algebra/4-line_up.py
+++ math/0x00-linear_algebra/4-line_up.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python3
+'''Line Up'''
+
+
 def add_arrays(arr1, arr2):
+    """adds two arrays element-wise"""
     if len(arr1) != len(arr2):
         return None
-    m =[]
+    m = []
     for i in range(len(arr1)):
         m.append(arr1[i] + arr2[i])
     return m
