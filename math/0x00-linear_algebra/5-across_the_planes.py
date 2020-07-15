639156820d862b74af564c810656e7c78b6c56d8
salmenz <salmen.zooro@gmail.com>
2020-07-15 14:30:45 +0100

    fix

--- math/0x00-linear_algebra/5-across_the_planes.py
+++ math/0x00-linear_algebra/5-across_the_planes.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python3
+'''Across The Planes'''
+
+
 def add_matrices2D(mat1, mat2):
+    """two matrices element-wise"""
     if len(mat2) != len(mat1):
         return None
     m = []
     for i in range(len(mat1)):
         if len(mat1[i]) != len(mat2[i]):
             return None
-        l = []
+        l1 = []
         for j in range(len(mat1[i])):
-            l.append(mat2[i][j]+mat1[i][j])
-        m.append(l)
+            l1.append(mat2[i][j]+mat1[i][j])
+        m.append(l1)
     return(m)
-    
-    
