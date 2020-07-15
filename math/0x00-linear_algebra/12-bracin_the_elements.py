d75e4c7100ab95bcbcd8d5a67178d0da82ea440f
salmenz <salmen.zooro@gmail.com>
2020-07-15 20:01:45 +0100

    fix output

--- math/0x00-linear_algebra/12-bracin_the_elements.py
+++ math/0x00-linear_algebra/12-bracin_the_elements.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
 '''racing The Elements'''
-import numpy as np
 
 
 def np_elementwise(mat1, mat2):
-    """ performs element-wise addition, subtraction, multiplication, and division"""
-    a = np.array(mat1)
-    b = np.array(mat2)
-    return (np.add(a, b), np.subtract(a, b), np.multiply(a, b), np.true_divide(a, b))
-    
+    """
+    performs element-wise addition,
+    subtraction, multiplication, and division
+    """
+    return (mat1+mat2, mat1-mat2, mat1*mat2, mat1/mat2)
