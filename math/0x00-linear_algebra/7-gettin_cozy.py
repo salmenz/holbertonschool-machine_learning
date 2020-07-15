b69c7248f6a7a6f9a1c0e0b3349a033e3e2cdfdf
salmenz <salmen.zooro@gmail.com>
2020-07-15 16:35:33 +0100

    fix

--- math/0x00-linear_algebra/7-gettin_cozy.py
+++ math/0x00-linear_algebra/7-gettin_cozy.py
@@ -6,10 +6,8 @@ def cat_matrices2D(mat1, mat2, axis=0):
     """that concatenates two matrices along a specific axis"""
     if axis == 0:
         return mat1+mat2
-    else if axis == 1:
+    else:
         for i in range(len(mat1)):
             for j in range(len(mat2[i])):
                 mat1[i].append(mat2[i][j])
         return mat1
-    else:
-        return None
