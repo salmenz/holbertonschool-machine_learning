e7fd643f2468912c8a715e024c4ea9347133a245
salmenz <salmen.zooro@gmail.com>
2020-07-15 19:53:26 +0100

    add and fix tasks

--- math/0x00-linear_algebra/10-ill_use_my_scale.py
+++ math/0x00-linear_algebra/10-ill_use_my_scale.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python3
+'''I’ll Use My Scale'''
 import numpy as np
+
+
 def np_shape(matrix):
+    """calculates the shape"""
     return matrix.shape
--- math/0x00-linear_algebra/12-bracin_the_elements.py
+++ math/0x00-linear_algebra/12-bracin_the_elements.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python3
+'''racing The Elements'''
 import numpy as np
+
+
 def np_elementwise(mat1, mat2):
+    """ performs element-wise addition, subtraction, multiplication, and division"""
     a = np.array(mat1)
     b = np.array(mat2)
     return (np.add(a, b), np.subtract(a, b), np.multiply(a, b), np.true_divide(a, b))
+    
