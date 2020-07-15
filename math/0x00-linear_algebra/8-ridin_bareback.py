f1ca99c1826e29b7a222d30ab4309dfee85b4971
salmenz <salmen.zooro@gmail.com>
2020-07-15 17:07:51 +0100

    fix

--- math/0x00-linear_algebra/8-ridin_bareback.py
+++ math/0x00-linear_algebra/8-ridin_bareback.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python3
+'''Ridin’ Bareback'''
 import numpy as np
+
+
 def mat_mul(mat1, mat2):
+    """performs matrix multiplication"""
     for l in mat1:
-        if len(l)!=len(mat1[0]):
+        if len(l) != len(mat1[0]):
             return None
-    if len(mat1[0]) != len (mat2):
+    if len(mat1[0]) != len(mat2):
         return None
     a = np.array(mat1)
     b = np.array(mat2)
