82177d188bb5789ca72a5e15eeac9548e2c363eb
salmenz <salmen.zooro@gmail.com>
2020-08-06 23:39:56 +0100

    fix

--- math/0x02-calculus/10-matisse.py
+++ math/0x02-calculus/10-matisse.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 """calculates the derivative of a polynomial"""
+
+
 def poly_derivative(poly):
+    """calculates the derivative of a polynomial"""
     if len(poly) == 1:
-        if  isinstance(poly[0], int):
+        if isinstance(poly[0], int):
             return [0]
         else:
             return None
@@ -11,7 +14,7 @@ def poly_derivative(poly):
     else:
         poly = poly[1:]
         for i in range(len(poly)):
-            if not isinstance(poly[0], int):
+            if not isinstance(poly[i], int):
                 return None
             else:
                 poly[i] *= i+1
