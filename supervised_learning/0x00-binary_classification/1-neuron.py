30f3568c97623a2e221bf49b2de4270abe5b08cb
salmenz <salmen.zooro@gmail.com>
2020-09-13 18:13:35 +0100

    neuron class

---
+++ supervised_learning/0x00-binary_classification/1-neuron.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""class neuron"""
+import numpy as np
+
+
+class Neuron:
+    """class neuron"""
+    def __init__(self, nx):
+        if type(nx) is not int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        self.nx = nx
+        self.__W = np.random.randn(1, nx)
+        self.__b = 0
+        self.__A = 0
+
+    @property
+    def W(self):
+        return self.__W
+    @property
+    def b(self):
+        return self.__b
+    @property
+    def A(self):
+        return self.__A
