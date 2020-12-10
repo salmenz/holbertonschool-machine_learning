a549fdf2ec1dcce65333b49db3a31878ee7de368
salmenz <salmen.zooro@gmail.com>
2020-12-10 22:11:17 +0100

    integrale

---
+++ math/0x02-calculus/17-integrate.py
@@ -0,0 +1,16 @@
+def poly_integral(poly, C=0):
+    if type(C) != int:
+        return None
+    if poly == []:
+        return None
+    for i in poly:
+        if type(i) != int:
+            return None
+    l1 = []
+    l1.append(C)
+    for i in range(1, len(poly)+1):
+        l1.append(poly[i-1]/i)
+    for i in range(len(l1)):
+        if l1[i] % 1 == 0:
+            l1[i] = int(l1[i])
+    return l1
