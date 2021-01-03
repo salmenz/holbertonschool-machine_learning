ca6de41e87d79b92b29db2098fe038d5812c8abc
salmenz <salmen.zooro@gmail.com>
2021-01-03 23:03:38 +0100

    forward and cost

--- supervised_learning/0x01-classification/10-neural_network.py
+++ supervised_learning/0x01-classification/10-neural_network.py
@@ -47,6 +47,7 @@ class NeuralNetwork:
         return self.__A2
 
     def forward_prop(self, X):
+        """forward propagation"""
         z1 = np.matmul(self.__W1, X) + self.__b1
         self.__A1 = 1 / (1 + np.exp(-z1))
         z2 = np.matmul(self.__W2, self.__A1) + self.__b2
