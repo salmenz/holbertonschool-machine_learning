1d39660485b7d099d8f4e6d347feba536c9fb5ac
salmenz <salmen.zooro@gmail.com>
2021-01-03 17:42:53 +0100

    add 6

--- supervised_learning/0x01-classification/6-neuron.py
+++ supervised_learning/0x01-classification/6-neuron.py
@@ -55,7 +55,7 @@ class Neuron:
     def train(self, X, Y, iterations=5000, alpha=0.05):
         if type(iterations) is not int:
             raise TypeError("iterations must be an integer")
-        if iterations < 1:
+        if iterations <= 0:
             raise ValueError("iterations must be a positive integer")
         if type(alpha) is not float:
             raise TypeError("alpha must be an float")
