9005914eaeb845430ba9cc6b954622d4edbb5313
salmenz <salmen.zooro@gmail.com>
2021-01-03 17:50:44 +0100

    add 6

--- supervised_learning/0x01-classification/6-neuron.py
+++ supervised_learning/0x01-classification/6-neuron.py
@@ -59,7 +59,7 @@ class Neuron:
         if iterations < 1:
             raise ValueError("iterations must be a positive integer")
         if type(alpha) is not float:
-            raise TypeError("alpha must be an float")
+            raise TypeError("alpha must be a float")
         if alpha <= 0:
             raise ValueError("alpha must be a positive")
         for i in range(iterations):
