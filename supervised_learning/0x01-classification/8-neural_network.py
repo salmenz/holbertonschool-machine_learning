a81a6e48f40e09e9bb7757ad271a8bf45072d05e
salmenz <salmen.zooro@gmail.com>
2021-01-03 20:47:26 +0100

    neuralnetwork

--- supervised_learning/0x01-classification/8-neural_network.py
+++ supervised_learning/0x01-classification/8-neural_network.py
@@ -16,9 +16,9 @@ class NeuralNetwork:
             raise ValueError("nodes must be a positive integer")
         self.nx = nx
         self.nodes = nodes
-        self.W1 = np.random.normal((nodes, nx))
+        self.W1 = np.random.normal(size=(nodes, nx))
         self.b1 = np.zeros((nodes, 1))
         self.A1 = 0
-        self.W2 = np.random.normal((1, nodes))
+        self.W2 = np.random.normal(size=(1, nodes))
         self.b2 = 0
         self.A2 = 0
