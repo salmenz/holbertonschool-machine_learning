e658b9d643a38e8a56b8c8ab60cf6f09aba9acec
salmenz <salmen.zooro@gmail.com>
2021-01-04 18:54:03 +0100

    evaluate

---
+++ supervised_learning/0x01-classification/12-neural_network.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+"""class NeuralNetwork"""
+import numpy as np
+
+
+class NeuralNetwork:
+    """class NeuralNetwork"""
+    def __init__(self, nx, nodes):
+        if type(nx) is not int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        if type(nodes) is not int:
+            raise TypeError("nodes must be an integer")
+        if nodes < 1:
+            raise ValueError("nodes must be a positive integer")
+
+        self.__W1 = np.random.normal(size=(nodes, nx))
+        self.__b1 = np.zeros((nodes, 1))
+        self.__A1 = 0
+        self.__W2 = np.random.normal(size=(1, nodes))
+        self.__b2 = 0
+        self.__A2 = 0
+
+    @property
+    def W1(self):
+        return self.__W1
+
+    @property
+    def b1(self):
+        return self.__b1
+
+    @property
+    def A1(self):
+        return self.__A1
+
+    @property
+    def W2(self):
+        return self.__W2
+
+    @property
+    def b2(self):
+        return self.__b2
+
+    @property
+    def A2(self):
+        return self.__A2
+
+    def forward_prop(self, X):
+        """forward propagation"""
+        z1 = np.matmul(self.__W1, X) + self.__b1
+        self.__A1 = 1 / (1 + np.exp(-z1))
+        z2 = np.matmul(self.__W2, self.__A1) + self.__b2
+        self.__A2 = 1 / (1 + np.exp(-z2))
+        return self.__A1, self.__A2
+
+    def cost(self, Y, A):
+        """calcul cost"""
+        s = (-Y) * np.log(A) - (1 - Y) * np.log(1.0000001 - A)
+        return np.sum(s)/Y.shape[1]
+
+    def evaluate(self, X, Y):
+        """Evaluates the neural network’s predictions"""
+        A2 = self.forward_prop(X)[1]
+        A3 = np.round(A2).astype(int)
+        return A3, self.cost(Y, A2)
