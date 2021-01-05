238e06385cd84a1aa91946b1cb39873e0cfffe0d
salmenz <salmen.zooro@gmail.com>
2021-01-06 00:52:42 +0100

    decode

--- supervised_learning/0x01-classification/25-one_hot_decode.py
+++ supervised_learning/0x01-classification/25-one_hot_decode.py
@@ -7,5 +7,4 @@ def one_hot_decode(one_hot):
     """converts a one-hot matrix into a vector of labels:"""
     if type(one_hot) is not np.ndarray or len(one_hot.shape) != 2:
         return None
-    return np.argmax(one_hot, axis=0)        
-
+    return np.argmax(one_hot, axis=0)
