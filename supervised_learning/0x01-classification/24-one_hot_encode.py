1ce4faab064eeb2a432ac28f2a8a84fe1ccbca4c
salmenz <salmen.zooro@gmail.com>
2021-01-05 23:47:15 +0100

    one hor encode

--- supervised_learning/0x01-classification/24-one_hot_encode.py
+++ supervised_learning/0x01-classification/24-one_hot_encode.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python3
+import numpy as np
+
+
 def one_hot_encode(Y, classes):
     """converts a numeric label vector into a one-hot matrix"""
-    
+    try:
+        b = np.zeros((Y.size, classes))
+        b[np.arange(Y.size), Y] = 1
+        return b.T
+    except Exception:
+        return None
