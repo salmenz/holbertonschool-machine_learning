25b5bea5bbcbf60601b50c720e21a3c0f64e4560
salmenz <salmen.zooro@gmail.com>
2021-01-05 18:27:36 +0100

    deep neural

--- supervised_learning/0x01-classification/17-deep_neural_network.py
+++ supervised_learning/0x01-classification/17-deep_neural_network.py
@@ -10,10 +10,8 @@ class DeepNeuralNetwork:
             raise TypeError("nx must be an integer")
         if nx < 1:
             raise ValueError("nx must be a positive integer")
-        if not isinstance(layers, list) or len(layers) == 0:
+        if not isinstance(layers, list) or not layers:
             raise TypeError("layers must be a list of positive integers")
-        if not all(map(lambda x: x > 0 and isinstance(x, int), layers)):
-            raise TypeError('layers must be a list of positive integers')
         self.nx = nx
         self.layers = layers
         self.L = len(layers)
@@ -22,7 +20,11 @@ class DeepNeuralNetwork:
         self.weights['W1'] = np.random.normal(size=(layers[0], nx)) \
             * np.sqrt(2/nx)
         self.weights['b1'] = np.zeros((layers[0], 1))
-        for i in range(1, self.L):
-            self.weights['W{i+1}'] = np.random.randn(layers[i], layers[i-1]) \
-                * np.sqrt(2/(layers[i] + layers[i-1]))
-            self.weights['b{i+1}'] = np.zeros((layers[i], 1))
+        for i in range(self.L):
+            if type(layers[i]) is not int or layers[i] < 1:
+                raise TypeError("layers must be a list of positive integers")
+            if i != 0:
+                self.weights['W' + str(i + 1)
+                             ] = np.random.randn(layers[i], layers[i-1]) \
+                     * np.sqrt(2/(layers[i] + layers[i-1]))
+                self.weights['b' + str(i + 1)] = np.zeros((layers[i], 1))
