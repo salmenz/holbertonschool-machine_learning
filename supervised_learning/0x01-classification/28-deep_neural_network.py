8c2587dec003d539ae4e86f742566efc9bfd16b3
salmenz <salmen.zooro@gmail.com>
2021-01-07 19:33:20 +0100

    sig & tanh

--- supervised_learning/0x01-classification/28-deep_neural_network.py
+++ supervised_learning/0x01-classification/28-deep_neural_network.py
@@ -7,31 +7,36 @@ import pickle
 
 class DeepNeuralNetwork:
     """class DeepNeuralNetwork"""
+
     def __init__(self, nx, layers, activation='sig'):
-        if type(nx) is not int:
+        if not isinstance(nx, int):
             raise TypeError("nx must be an integer")
-        if nx < 1:
+        elif nx <= 0:
             raise ValueError("nx must be a positive integer")
         if not isinstance(layers, list) or not layers:
             raise TypeError("layers must be a list of positive integers")
         if activation not in ('sig', 'tanh'):
             raise ValueError("activation must be 'sig' or 'tanh'")
         self.__L = len(layers)
-        self.__activation = activation
         self.__cache = {}
         self.__weights = {}
-        self.__weights['W1'] = np.random.normal(size=(layers[0], nx)) \
-            * np.sqrt(2/nx)
-        self.__weights['b1'] = np.zeros((layers[0], 1))
-        for i in range(self.L):
-            if type(layers[i]) is not int or layers[i] < 1:
+        self.__activation = activation
+        for i in range(len(layers)):
+            if not isinstance(layers[i], int) or layers[i] <= 0:
                 raise TypeError("layers must be a list of positive integers")
-            if i != 0:
+            if i == 0:
+                self.__weights['b' + str(i + 1)] = np.zeros((layers[0], 1))
                 self.__weights['W' + str(i + 1)
-                               ] = np.random.normal(size=(layers[i],
-                                                          layers[i-1])) \
-                     * np.sqrt(2/(layers[i-1]))
+                               ] = np.random.normal(size=(layers[i], nx)
+                                                    ) * np.sqrt(2/nx)
+
+            else:
                 self.__weights['b' + str(i + 1)] = np.zeros((layers[i], 1))
+                self.__weights[
+                    'W' + str(i + 1)
+                ] = np.random.normal(size=(layers[i], layers[i-1])
+                                     )*np.sqrt(
+                    2/layers[i-1])
 
     @property
     def L(self):
