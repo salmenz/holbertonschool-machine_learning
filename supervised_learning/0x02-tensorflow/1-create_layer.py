9aebfe1f9c39cdcf3f6deb008751e468087fb0b3
salmenz <salmen.zooro@gmail.com>
2021-01-07 21:33:49 +0100

    create layer

--- supervised_learning/0x02-tensorflow/1-create_layer.py
+++ supervised_learning/0x02-tensorflow/1-create_layer.py
@@ -7,6 +7,6 @@ import tensorflow as tf
 def create_layer(prev, n, activation):
     """create layer with tensorflow"""
     kernel = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
-    la = tf.layers.dense(units=n, activation=activation, name="layer",
-                         kernel_initializer=kernel)
+    la = tf.layers.Dense(prev, units=n, activation=activation, name="layer",
+                         kernel_initializer=kernel, reuse=tf.AUTO_REUSE)
     return layer(la)
