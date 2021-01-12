aab824d1044f3f116959d3495d81b373c5db39f4
salmenz <salmen.zooro@gmail.com>
2021-01-12 22:26:55 +0100

    fix pycodestyle

--- supervised_learning/0x03-optimization/8-RMSProp.py
+++ supervised_learning/0x03-optimization/8-RMSProp.py
@@ -5,4 +5,5 @@ import tensorflow as tf
 
 def create_RMSProp_op(loss, alpha, beta2, epsilon):
     """updates a variable using the RMSProp optimization algorithm"""
-    return tf.train.RMSPropOptimizer(alpha, beta2, epsilon=epsilon).minimize(loss)
+    return tf.train.RMSPropOptimizer(alpha, beta2,
+                                     epsilon=epsilon).minimize(loss)
