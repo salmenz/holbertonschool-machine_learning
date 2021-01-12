a900b73a8b6da7a1b9cd68f6aca1123e5ffdbe3f
salmenz <salmen.zooro@gmail.com>
2021-01-13 00:40:24 +0100

    decay tensorflow

--- supervised_learning/0x03-optimization/12-learning_rate_decay.py
+++ supervised_learning/0x03-optimization/12-learning_rate_decay.py
@@ -8,4 +8,4 @@ def learning_rate_decay(alpha, decay_rate, global_step, decay_step):
     """creates a learning rate decay operation in tensorflow
      using inverse time decay"""
     return tf.train.inverse_time_decay(alpha, global_step,
-                                       decay_step, decay_rate)
+                                       decay_step, decay_rate, staircase=True)
