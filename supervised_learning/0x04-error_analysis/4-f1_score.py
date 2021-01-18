dd7d8cff6df333b5f8ef6708499664c8d915a1ad
salmenz <salmen.zooro@gmail.com>
2021-01-18 12:48:26 +0100

    replace code with import

--- supervised_learning/0x04-error_analysis/4-f1_score.py
+++ supervised_learning/0x04-error_analysis/4-f1_score.py
@@ -1,16 +1,11 @@
 #!/usr/bin/env python3
 """calculates the F1 score for each class in a precision matrix"""
 import numpy as np
+sensitivity = __import__('1-sensitivity').sensitivity
+precision = __import__('2-precision').precision
 
 
 def f1_score(confusion):
     """calculates the F1 score for each class in a precision matrix"""
-    t = np.zeros(confusion.shape[0])
-    t1 = np.zeros(confusion.shape[0])
-    c = confusion.T
-    i = 0
-    for cl, cl1 in zip(c, confusion):
-        t[i] = cl[i] / cl.sum()
-        t1[i] = cl1[i] / cl1.sum()
-        i += 1
-    return 2 * t * t1 / (t1 + t)
+    return 2 * ((precision(confusion) * sensitivity(confusion)) /
+                (precision(confusion) + sensitivity(confusion)))
