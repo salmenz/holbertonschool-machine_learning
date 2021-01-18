4271e29aebf7315dd4cba5be97623b69999414bd
salmenz <salmen.zooro@gmail.com>
2021-01-18 10:40:57 +0100

    F1 score

---
+++ supervised_learning/0x04-error_analysis/4-f1_score.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""calculates the F1 score for each class in a precision matrix"""
+import numpy as np
+
+
+def f1_score(confusion):
+    """calculates the F1 score for each class in a precision matrix"""
+    t = np.zeros(confusion.shape[0])
+    t1 = np.zeros(confusion.shape[0])
+    c = confusion.T
+    i = 0
+    for cl, cl1 in zip(c, confusion):
+        t[i] = cl[i] / cl.sum()
+        t1[i] = cl[i] / cl1.sum()
+        i += 1
+    return 2 * t * t1 / (t1 + t)
