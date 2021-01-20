5300172ddb494eec10c20628d26503e3b2036dcd
salmenz <salmen.zooro@gmail.com>
2021-01-20 18:19:51 +0100

    fix

--- supervised_learning/0x05-regularization/2-l2_reg_cost.py
+++ supervised_learning/0x05-regularization/2-l2_reg_cost.py
@@ -5,4 +5,5 @@ import tensorflow as tf
 
 def l2_reg_cost(cost):
     """creates a tensorflow layer that includes L2 regularization"""
-    return tf.get_collection(tf.GraphKeys.REGULARIZATION_LOSSES) + cost
+    loss = tf.losses.get_regularization_losses()
+    return cost + loss
