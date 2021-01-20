046bd6449d42ff6244c83ed0a0514d9ad84e4163
salmenz <salmen.zooro@gmail.com>
2021-01-20 17:51:30 +0100

    fix

--- supervised_learning/0x05-regularization/5-dropout_gradient_descent.py
+++ supervised_learning/0x05-regularization/5-dropout_gradient_descent.py
@@ -8,14 +8,14 @@ def dropout_gradient_descent(Y, weights, cache, alpha, keep_prob, L):
     """updates the weights of a neural network with Dropout
     regularization using gradient descent"""
     m = Y.shape[1]
-    weights = weights.copy()
+    weights_cpy = weights.copy()
     dz = cache['A' + str(L)] - Y
     for i in range(L, 0, -1):
         dwn = np.matmul(dz, cache['A' + str(i - 1)].T) * 1 / m
         dbn = np.sum(dz, axis=1, keepdims=True) * 1 / m
         weights['W' + str(i)] = weights['W' + str(i)] - dwn * alpha
         weights['b' + str(i)] = weights['b' + str(i)] - dbn * alpha
-        dA = cache['A' + str(i - 1)] * (1 - cache['A'+str(i-1)])
+        dA = 1 - cache['A' + str(i - 1)] * cache['A'+str(i-1)]
         if i > 1:
-            dz = np.matmul(weights['W' + str(i)].T, dz) * dA * \
+            dz = np.matmul(weights_cpy['W' + str(i)].T, dz) * dA * \
                  cache['D' + str(i-1)] / keep_prob
