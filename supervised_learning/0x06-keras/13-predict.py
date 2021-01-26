4b328e36a601ae6838897a16a0fffa266c841a3e
salmenz <salmen.zooro@gmail.com>
2021-01-26 22:36:51 +0100

    make the predection

---
+++ supervised_learning/0x06-keras/13-predict.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""makes a prediction using a neural network"""
+import tensorflow.keras as k
+
+
+def predict(network, data, verbose=False):
+    """makes a prediction using a neural network"""
+    return = network.predict(x=data, verbose=verbose)
