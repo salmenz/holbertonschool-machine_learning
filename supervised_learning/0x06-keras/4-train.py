f86add7b9ead1925d71f52be1ca62f9163c325a3
salmenz <salmen.zooro@gmail.com>
2021-01-26 21:30:01 +0100

    fix codestyle

--- supervised_learning/0x06-keras/4-train.py
+++ supervised_learning/0x06-keras/4-train.py
@@ -6,5 +6,5 @@ import tensorflow.keras as k
 def train_model(network, data, labels, batch_size,
                 epochs, verbose=True, shuffle=False):
     """trains a model using mini-batch gradient descent"""
-    return network.fit(data, labels, batch_size=batch_size,epochs=epochs,
+    return network.fit(data, labels, batch_size=batch_size, epochs=epochs,
                        verbose=verbose, shuffle=shuffle)
