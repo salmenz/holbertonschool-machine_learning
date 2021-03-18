529320fe104fd16ae337bf480043338ce5674cbd
salmenz <salmen.zooro@gmail.com>
2021-03-18 15:38:52 +0100

    calculate minor or a matrix

--- math/0x05-advanced_linear_algebra/1-minor.py
+++ math/0x05-advanced_linear_algebra/1-minor.py
@@ -17,6 +17,15 @@ def drop(matrix, i, j):
 
 def determinant(matrix):
     """calculates the determinant of a matrix"""
+    if matrix == [[]]:
+        return 1
+    if type(matrix) != list or len(matrix) == 0:
+        raise TypeError("matrix must be a list of lists")
+    for i in matrix:
+        if type(i) != list:
+            raise TypeError("matrix must be a list of lists")
+        if len(matrix) != len(i):
+            raise ValueError("matrix must be a square matrix")
     if len(matrix) == 1:
         return matrix[0][0]
     if len(matrix) == 2:
