bca83acf0d8b030fff61fb00a76941f832939c2f
salmenz <salmen.zooro@gmail.com>
2021-03-22 22:33:43 +0100

    calculates a correlation matrix

--- math/0x06-multivariate_prob/1-correlation.py
+++ math/0x06-multivariate_prob/1-correlation.py
@@ -5,8 +5,11 @@ import numpy as np
 
 def correlation(C):
     """calculates a correlation matrix"""
-    if len(X.shape) != 2:
+    if not isinstance(C, np.ndarray):
         raise TypeError("C must be a numpy.ndarray")
     if C.shape[0] != C.shape[1]:
         raise ValueError("C must be a 2D square matrix")
-    
+    variance = np.sqrt(np.diag(C))
+    outer = np.outer(variance, variance)
+    C = C / outer
+    return C
