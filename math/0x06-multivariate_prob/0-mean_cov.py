ad8440b98b09542ce7f17b5b3bde619d6120fb43
salmenz <salmen.zooro@gmail.com>
2021-03-22 19:31:17 +0100

    calculates the mean and covariance of a data set

---
+++ math/0x06-multivariate_prob/0-mean_cov.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""calculates the mean and covariance of a data set"""
+import numpy as np
+
+
+def mean_cov(X):
+    """calculates the mean and covariance of a data set"""
+    if len(X.shape) != 2:
+        raise TypeError("X must be a 2D numpy.ndarray")
+    if X.shape[0] < 2:
+        raise ValueError("X must contain multiple data points")
+    mean = np.transpose(X).mean(axis=1)
+    return mean, np.matmul(np.transpose(X - mean), X - mean)/(X.shape[0] - 1)
