80c64e5e6853cdfe8d3de034baea6ff2fd89be20
salmenz <salmen.zooro@gmail.com>
2021-03-22 23:46:16 +0100

    calculates the PDF at a data point

--- math/0x06-multivariate_prob/multinormal.py
+++ math/0x06-multivariate_prob/multinormal.py
@@ -16,3 +16,22 @@ class MultiNormal:
         self.mean = data.mean(axis=1, keepdims=True)
         self.cov = np.matmul(data - self.mean, np.transpose(data - self.mean))\
             / (data.shape[1] - 1)
+    
+
+    def pdf(self, x):
+        """calculates the PDF at a data point"""
+        if not isinstance(x, np.ndarray):
+            raise TypeError('x must be a numpy.ndarray')
+        d = self.cov.shape[0]
+        if len(x.shape) != 2:
+            raise ValueError('x must have the shape ({}, 1)'.format(d))
+        if x.shape[1] != 1 or x.shape[0] != d:
+            raise ValueError('x must have the shape ({}, 1)'.format(d))
+        n = x.shape[0]
+        m = self.mean
+        c = self.cov
+        den = np.sqrt(((2 * np.pi) ** n) * np.linalg.det(c))
+        icov = np.linalg.inv(c)
+        expo = (-0.5 * np.matmul(np.matmul((x - m).T, icov), x - self.mean))
+        pdf = (1 / den) * np.exp(expo[0][0])
+        return pdf
