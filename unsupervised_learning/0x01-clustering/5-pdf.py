2e4ad2f587b1fe69c9a2b287a875f1c51327cb28
salmenz <salmen.zooro@gmail.com>
2021-04-01 17:15:04 +0100

    calculates the probability density function of a Gaussian distribution

--- unsupervised_learning/0x01-clustering/5-pdf.py
+++ unsupervised_learning/0x01-clustering/5-pdf.py
@@ -6,6 +6,16 @@ import numpy as np
 def pdf(X, m, S):
     """calculates the probability density
     function of a Gaussian distribution"""
+    if type(X) is not np.ndarray or len(X.shape) != 2:
+        return None
+    if type(m) is not np.ndarray or len(m.shape) != 1:
+        return None
+    if type(S) is not np.ndarray or len(S.shape) != 2:
+        return None
+    if X.shape[1] != m.shape[0] or X.shape[1] != S.shape[0]:
+        return None
+    if S.shape[1] != S.shape[0]:
+        return None
     m1 = X - m
     inv_S = np.linalg.inv(S)
     mul1 = np.matmul(m1, inv_S)
