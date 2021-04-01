798684450fc0653ee5d38bc4027608edc6808e3b
salmenz <salmen.zooro@gmail.com>
2021-04-01 17:13:46 +0100

    calculates the probability density function of a Gaussian distribution

--- unsupervised_learning/0x01-clustering/5-pdf.py
+++ unsupervised_learning/0x01-clustering/5-pdf.py
@@ -8,6 +8,7 @@ def pdf(X, m, S):
     function of a Gaussian distribution"""
     m1 = X - m
     inv_S = np.linalg.inv(S)
-    var1 = np.exp((np.matmul(np.matmul(m1, inv_S), m1.T)) * -0.5)
+    mul1 = np.matmul(m1, inv_S)
+    var1 = np.exp(np.matmul(mul1, m1.T).diagonal() * -0.5)
     var2 = (((2 * np.pi) ** X.shape[1]) * np.linalg.det(S)) ** (0.5)
     return var1/var2
