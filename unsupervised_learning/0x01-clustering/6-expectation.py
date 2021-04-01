bc31eca0519c92ced99acef5e45f234f88c661f9
salmenz <salmen.zooro@gmail.com>
2021-04-01 22:43:38 +0100

    the expectation step in the EM algorithm

--- unsupervised_learning/0x01-clustering/6-expectation.py
+++ unsupervised_learning/0x01-clustering/6-expectation.py
@@ -7,13 +7,15 @@ pdf = __import__('5-pdf').pdf
 def expectation(X, pi, m, S):
     """calculates the expectation step in the EM algorithm for a GMM"""
     if type(X) is not np.ndarray or len(X.shape) != 2:
-        return (None, None)
-    if type(m) is not np.ndarray or len(m.shape) != 2:
-        return (None, None)
-    if type(S) is not np.ndarray or len(S.shape) != 3:
-        return (None, None)
+        return None, None
     if type(pi) is not np.ndarray or len(pi.shape) != 1:
-        return (None, None)
+        return None, None
+    if not np.isclose(pi.sum(), 1):
+        return None, None
+    if type(m) is not np.ndarray or len(m.shape) != 2:
+        return None, None
+    if type(S) is not np.ndarray or len(S.shape) != 3:
+        return None, None
     k = pi.shape[0]
     if (k, X.shape[1]) != m.shape or (k, X.shape[1], X.shape[1]) != S.shape:
         return None, None
