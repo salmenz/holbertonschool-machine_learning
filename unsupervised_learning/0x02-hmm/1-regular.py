7ffa116a9a0f7226d813ff30b08666f096021627
salmenz <salmen.zooro@gmail.com>
2021-04-06 13:18:23 +0100

    fix

--- unsupervised_learning/0x02-hmm/1-regular.py
+++ unsupervised_learning/0x02-hmm/1-regular.py
@@ -5,12 +5,7 @@ import numpy as np
 
 def regular(P):
     """determines the steady state probabilities of a regular markov chain"""
-    if type(P) != np.ndarray or len(P.shape) != 2 or P.shape[0] != P.shape[1]:
-        return None
-    if not np.array_equal(np.sum(P, axis=1), np.ones(P.shape[0])):
-        return None
-    if np.max(P) > 1 or np.min(P) < 0:
-        return None
+
     if np.any(P == 0):
         return None
     s = np.zeros(P.shape[0])
