d5040f617b4822d4fcd5e0a130e0e16f6af114db
salmenz <salmen.zooro@gmail.com>
2021-04-07 10:55:53 +0100

    determines the steady state probabilities of a regular markov chain

--- unsupervised_learning/0x02-hmm/1-regular.py
+++ unsupervised_learning/0x02-hmm/1-regular.py
@@ -5,11 +5,16 @@ import numpy as np
 
 def regular(P):
     """determines the steady state probabilities of a regular markov chain"""
-
-    if np.any(P == 0):
+    if type(P) != np.ndarray or len(P.shape) != 2 or P.shape[0] != P.shape[1]:
         return None
-    s = np.zeros(1, P.shape[0])
-    s[0, 0] = 1
+    if not np.array_equal(np.sum(P, axis=1), np.ones(P.shape[0])):
+        return None
+    if np.max(P) > 1 or np.min(P) < 0:
+        return None
+    if not np.all(P > 0):
+        return None
+    s = np.zeros(P.shape[0])
+    s[0] = 1
     while not np.array_equal(np.matmul(s, P), s):
         s = np.matmul(s, P)
-    return s
+    return s[np.newaxis]
