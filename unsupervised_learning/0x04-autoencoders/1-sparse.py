f89ddb85a45e3c9a9b7c9d34d9f5d7649ffec118
salmenz <salmen.zooro@gmail.com>
2021-04-21 23:17:05 +0100

    sparse autoencoder

---
+++ unsupervised_learning/0x04-autoencoders/1-sparse.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+"""sparse autoencoder"""
+import tensorflow.keras as K
+
+
+def autoencoder(input_dims, hidden_layers, latent_dims, lambtha):
+    """sparse autoencoder"""
+    # encoder
+    enc_in = K.Input(shape=(input_dims,))
+    k_r = K.regularizers.l1(lambtha)
+    for i in range(len(hidden_layers)):
+        if i == 0:
+            y = (K.layers.Dense(hidden_layers[i], activation="relu",
+                 kernel_regularizer=k_r))(enc_in)
+        else:
+            y = (K.layers.Dense(hidden_layers[i], activation="relu",
+                 kernel_regularizer=k_r))(y)
+
+    y = (K.layers.Dense(latent_dims, activation="relu",
+         kernel_regularizer=k_r))(y)
+    
+    encoder = K.Model(enc_in, y)
+
+    # decoder
+    dec_in = K.Input(shape=(latent_dims,))
+    for i in range(len(hidden_layers)-1, -1, -1):
+        if i == len(hidden_layers)-1:
+            y = (K.layers.Dense(hidden_layers[i], activation="relu"))(dec_in)
+        else:
+            y = (K.layers.Dense(hidden_layers[i], activation="relu"))(y)
+    y = (K.layers.Dense(input_dims, activation="sigmoid"))(y)
+    decoder = K.Model(dec_in, y)
+
+    out_encoder = encoder(enc_in)
+    out_decoder = decoder(out_encoder)
+
+    auto = K.Model(enc_in, out_decoder)
+    auto.compile(optimizer='adam', loss='binary_crossentropy')
+    return encoder, decoder, auto
