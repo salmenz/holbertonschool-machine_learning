3f5ed27127dfb97b1f32b0faf9b94db89ad3d333
salmenz <salmen.zooro@gmail.com>
2021-05-12 16:24:09 +0100

    unigram BLUE score Calculation

---
+++ supervised_learning/0x10-nlp_metrics/0-uni_bleu.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""calculates the unigram BLEU score for a sentence"""
+import numpy as np
+
+
+def uni_bleu(references, sentence):
+    """calculates the unigram BLEU score for a sentence"""
+    len_sen = len(sentence)
+    min = len(sentence)
+    sum_count_clip = 0
+    for word in sentence:
+        count_clip = 0
+        for ref in references:
+            if ref.count(word) > count_clip:
+                count_clip = ref.count(word)
+            if abs(len(ref) - len_sen) < min:
+                min = abs(len(ref) - len_sen)
+                closest_len = len(ref)
+        sum_count_clip += count_clip
+    bp = np.exp(1 - float(closest_len) / float(len_sen))
+    if len_sen > closest_len:
+        bp = 1
+    return bp * np.exp(np.log(sum_count_clip / len_sen))
