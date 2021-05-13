6f8a339de55a6b794495e1370fd8c9507140a709
salmenz <salmen.zooro@gmail.com>
2021-05-13 23:47:46 +0100

    unigram BLUE score calculation

--- supervised_learning/0x10-nlp_metrics/0-uni_bleu.py
+++ supervised_learning/0x10-nlp_metrics/0-uni_bleu.py
@@ -1,29 +1,23 @@
-
 #!/usr/bin/env python3
-
-""" contains uni_bleu function"""
+"""calculates the unigram BLEU score for a sentence"""
 import numpy as np
 
 
 def uni_bleu(references, sentence):
-    """
-    Calculates the unigram BLEU
-    score for a sentence
-    """
-    c = len(sentence)
-    r = np.array([len(r) for r in references])
-    r = np.argmin(np.abs(r - c))
-    r = len(references[r])
-    bp = 1
-    if r > c:
-        bp = np.exp(1 - r / c)
-    words = {}
+    """calculates the unigram BLEU score for a sentence"""
+    len_sen = len(sentence)
+    min = max(len_sen, max(len(i) for i in references))
+    sum_count_clip = 0
     for word in sentence:
+        count_clip = 0
         for ref in references:
-            if word in words:
-                if words[word] < ref.count(word):
-                    words.update({word: ref.count(word)})
-            else:
-                words.update({word: ref.count(word)})
-    p = sum(words.values()) / c
-    return bp * p
+            if ref.count(word) > count_clip:
+                count_clip = ref.count(word)
+            if abs(len(ref) - len_sen) < min:
+                min = abs(len(ref) - len_sen)
+                closest_len = len(ref)
+        sum_count_clip += count_clip
+    bp = np.exp(1 - closest_len / len_sen)
+    if len_sen > closest_len:
+        bp = 1
+    return bp * sum_count_clip / len_sen
