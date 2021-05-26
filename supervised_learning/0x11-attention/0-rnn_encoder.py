d24db2f9bb4cdd8727b89b6c9906df91c38a2b82
salmenz <salmen.zooro@gmail.com>
2021-05-26 15:25:34 +0100

    rnn encoder class

---
+++ supervised_learning/0x11-attention/0-rnn_encoder.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+import tensorflow as tf
+"""class RNNEncoder"""
+
+
+class RNNEncoder(tf.keras.layers.Layer):
+    """class RNNEncoder"""
+    def __init__(self, vocab, embedding, units, batch):
+        super(RNNEncoder, self).__init__()
+        self.batch = batch
+        self.units = units
+        self.embedding = tf.keras.layers.Embedding(embedding, units,
+                                                   input_length=vocab)
+        self.gru = tf.keras.layers.GRU(units, return_sequences=True,
+                                       kernel_initializer="glorot_uniform",
+                                       return_state=True)
+
+    def initialize_hidden_state(self):
+        """call function"""
+        return tf.zeros((self.batch, self.units))
+
+    def call(self, x, initial):
+        """call function"""
+        x = self.embedding(x)
+        outputs, hidden = self.gru(x, initial_state=initial)
+        return outputs, hidden
