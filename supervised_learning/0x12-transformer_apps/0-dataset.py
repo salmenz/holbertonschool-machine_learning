454d497f607e1bcae1f12ac40180a83a18209cce
salmenz <salmen.zooro@gmail.com>
2021-05-28 15:40:27 +0100

    loads and preps a dataset

--- supervised_learning/0x12-transformer_apps/0-dataset.py
+++ supervised_learning/0x12-transformer_apps/0-dataset.py
@@ -16,8 +16,8 @@ class Dataset:
 
     def tokenize_dataset(self, data):
         """creates sub-word tokenizers for our dataset"""
-        tok_pt = tfds.deprecated.text.SubwordTextEncoder.build_from_corpus(
+        token_pt = tfds.deprecated.text.SubwordTextEncoder.build_from_corpus(
             (pt.numpy() for pt, _ in data), target_vocab_size=2**15)
-        tok_en = tfds.deprecated.text.SubwordTextEncoder.build_from_corpus(
+        token_en = tfds.deprecated.text.SubwordTextEncoder.build_from_corpus(
             (en.numpy() for _, en in data), target_vocab_size=2**15)
-        return tok_pt, tok_en
+        return token_pt, token_en
