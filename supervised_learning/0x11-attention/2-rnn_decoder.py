69a0d336464ec729c003cdbda07ca8fbbf6b4388
salmenz <salmen.zooro@gmail.com>
2021-06-16 00:27:43 +0100

    fix

--- supervised_learning/0x11-attention/2-rnn_decoder.py
+++ supervised_learning/0x11-attention/2-rnn_decoder.py
@@ -1,17 +1,16 @@
 #!/usr/bin/env python3
-"""RNN Decoder"""
+"""class RNNDecoder"""
 import tensorflow as tf
 SelfAttention = __import__('1-self_attention').SelfAttention
 
 
 class RNNDecoder(tf.keras.Model):
-    """RNNDecoder class"""
+    """class RNNDecoder"""
     def __init__(self, vocab, embedding, units, batch):
         """class constructor"""
         super(RNNDecoder, self).__init__()
         self.embedding = tf.keras.layers.Embedding(vocab, embedding)
-        self.gru = tf.keras.layers.GRU(units,
-                                       return_sequences=True,
+        self.gru = tf.keras.layers.GRU(units, return_sequences=True,
                                        return_state=True,
                                        recurrent_initializer='glorot_uniform')
         self.F = tf.keras.layers.Dense(vocab)
@@ -19,11 +18,11 @@ class RNNDecoder(tf.keras.Model):
         self.attention = SelfAttention(units)
 
     def call(self, x, s_prev, hidden_states):
-        """decode for machine translation:"""
-        context_vector, _ = self.attention(s_prev, hidden_states)
+        """call function"""
+        context_v, _ = self.attention(s_prev, hidden_states)
+        context_v = tf.expand_dims(context_v, 1)
         x = self.embedding(x)
-        context_vector = tf.expand_dims(context_vector, 1)
-        x = tf.concat([context_vector, x], axis=-1)
+        x = tf.concat([context_v, x], axis=-1)
         output, state = self.gru(x)
         output = tf.reshape(output, (-1, output.shape[2]))
         x = self.F(output)
