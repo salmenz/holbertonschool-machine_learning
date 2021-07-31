b22ee73bd0e8bd3c89a649a1db105e0f4d0d2f53
salmenz <salmen.zooro@gmail.com>
2021-07-31 18:22:26 +0100

    add comments

--- pipeline/0x02-databases/5-average.sql
+++ pipeline/0x02-databases/5-average.sql
@@ -1,3 +1,3 @@
 -- computes the score average of all records in the table second_table in your MySQL server
 
-SELECT CAST(AVG(score) AS DECIMAL(10,2)) AS average FROM second_table;
+SELECT AVG(score AS average FROM second_table;
