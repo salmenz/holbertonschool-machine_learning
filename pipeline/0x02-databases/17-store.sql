fe66e93a3dd8d87861dc06228584f3eba0cdcf9d
salmenz <salmen.zooro@gmail.com>
2021-08-01 21:08:36 +0100

    Databases

--- pipeline/0x02-databases/17-store.sql
+++ pipeline/0x02-databases/17-store.sql
@@ -1,9 +1,12 @@
 -- trigger that decreases the quantity of an item after adding a new order
 
+DELIMITER //
 CREATE TRIGGER update_items
   AFTER INSERT
   ON orders
   FOR EACH ROW
+  -- test
 BEGIN
   UPDATE items SET quantity = quantity - new.number WHERE items.name=new.item_name;  
-END
+END//
+DELIMITER ;
