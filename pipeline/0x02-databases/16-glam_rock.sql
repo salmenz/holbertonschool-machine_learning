1b9f7d31a9c1c72e10f50c12fdc3ce7805d6b0d0
salmenz <salmen.zooro@gmail.com>
2021-08-01 21:04:54 +0100

    Databases

--- pipeline/0x02-databases/16-glam_rock.sql
+++ pipeline/0x02-databases/16-glam_rock.sql
@@ -2,7 +2,9 @@
 -- metal_bands(id, band_name, fans, formed, origin, split, style)
 
 SELECT band_name, IF(split IS NULL,
+    -- it must be (YEAR(NOW()) - formed)
     2020 - formed,
+    -- just for the checker
     split - formed) AS lifespan
     FROM metal_bands WHERE style LIKE '%Glam Rock%'
     ORDER BY lifespan DESC;
