2fdc34728e113166870892a0a0cc7dc95f9550b9
salmenz <salmen.zooro@gmail.com>
2021-08-02 15:53:14 +0100

    Databases

--- pipeline/0x02-databases/20-average_score.sql
+++ pipeline/0x02-databases/20-average_score.sql
@@ -3,8 +3,8 @@
 DELIMITER //
 CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
     BEGIN
-        UPDATE users SET average_score=(SELECT AVG(score)
-        FROM corrections WHERE user_id=user_id)
-        WHERE id=user_id;
+        SELECT AVG(score) INTO @avg FROM corrections
+        WHERE corrections.user_id = user_id;
+        UPDATE users SET average_score = @avg WHERE id = user_id;
     END//
 DELIMITER ;
