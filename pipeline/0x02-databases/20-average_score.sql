c801d491b022dee1af8e7b5131cbeaf22a0d597b
salmenz <salmen.zooro@gmail.com>
2021-08-02 15:20:28 +0100

    Databases

--- pipeline/0x02-databases/20-average_score.sql
+++ pipeline/0x02-databases/20-average_score.sql
@@ -6,5 +6,5 @@ CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
         UPDATE users SET average_score=(SELECT AVG(score)
         FROM corrections WHERE user_id=user_id)
         WHERE id=user_id;
-    END //
+    END//
 DELIMITER ;
